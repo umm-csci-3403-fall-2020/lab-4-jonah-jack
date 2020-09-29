b531d08ac8869bc37dfd926e358afcd525d8f1c9
Jack Perala <peralaj94@gmail.com>
2020-09-29 11:00:51 -0500

    Write the boolean function for identifying vowels
    Wrote the boolean function for identifying vowels.

---
+++ file_disemvowel/file_disemvowel.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Declarations
+bool isVowel(char x);
+
+
+
+
+
+bool is Vowel(char x) {
+	if (x == 'a' ||
+	    x == 'e' ||
+ 	    x == 'i' ||
+ 	    x == 'o' ||
+ 	    x == 'u' ||
+ 	    x == 'A' ||
+ 	    x == 'E' ||
+ 	    x == 'I' ||
+ 	    x == 'O' ||
+ 	    x == 'U' ){
+		return 1;
+	}
+		return 0;
+}
+
