9a6e675cb0416c353883b5e9a76a364537b15379
Jack Perala <peralaj94@gmail.com>
2020-10-01 11:04:46 -0500

    Re-add the main method and stubs
    Re-added the main method and stubs in file_disemvowel.c to rectify compiling issues.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define BUF_SIZE 1024
 
@@ -10,7 +11,7 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf);
 
 
 
-bool is Vowel(char x) {
+bool isVowel(char x) {
 	if (x == 'a' ||
 	    x == 'e' ||
  	    x == 'i' ||
@@ -28,5 +29,29 @@ bool is Vowel(char x) {
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
 
+	// Variable to keep track of the index in the out_buf array
+	int index = 0;
+	int i;
+	// Looping through the given in_buf array to find the length of the strings.	
+	for (i = 0; in_buf[i] != '\0'; ++i) {
+		if (!isVowel(in_buf[i])) {
+			out_buf[index] = in_buf[i];
+			++index;
+		}
+	}
+
+	return index;
 }
 
+void disemvowel(FILE* inputFile, FILE* outputFILE) {
+
+}
+
+int main(int argc, char*argv[]) {
+	FILE *inputFile;
+	FILE *outputFile;
+
+	disemvowel(inputFile, outputFile);
+
+	return 0;
+}
