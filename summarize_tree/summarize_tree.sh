6324be547c24b52501236a400660924c48970bef
Jack Perala <peralaj94@gmail.com>
2020-10-06 18:19:37 -0500

    Add find methods for directories and files.
    Added find methods and variables to count the number of directories and files.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,5 +1,8 @@
 #usr/bin/env bash
 
+dirCount = $(find $1 -type d | wc -1)
+
+regFileCount = $(find $1 -type f| wc -1)
 
 echo "Processed all the files from <###PLACEHOLDER###>."
 echo "There were ###PLACEHODLER### directories."
