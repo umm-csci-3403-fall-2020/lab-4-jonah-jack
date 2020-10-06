4785ddf81ce94c18ad61b9f7caadb55f3654e2b8
Jack Perala <peralaj94@gmail.com>
2020-10-06 18:39:12 -0500

    Correct the wc flag.
    Corrected the wc flag to an l ("ell") from a 1 ("one").

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,9 +1,11 @@
 #usr/bin/env bash
 
-dirCount = $(find $1 -type d | wc -1)
+PWD = $1
 
-regFileCount = $(find $1 -type f| wc -1)
+dirCount = $(find $1 -type d | wc -l)
 
-echo "Processed all the files from <###PLACEHOLDER###>."
-echo "There were ###PLACEHODLER### directories."
-echo "There were ###PLACEHOLDER### files."
+regFileCount = $(find $1 -type f| wc -l)
+
+echo "Processed all the files from $PWD."
+echo "There were $dirCount directories."
+echo "There were $regFileCount files."
